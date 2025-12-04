1aa2dda144b4642798bdf1a37c29570ab45b76c0
Ross Golder <ross@golder.org>
2025-12-04 09:14:32 +0700

    chore: bump version to v0.6.9

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM golang:1.25-alpine AS builder
 ARG TARGETOS
 ARG TARGETARCH
-ARG VERSION=v0.6.8
+ARG VERSION=v0.6.9
 ARG COMMIT=unknown
 ARG DATE=unknown
 
--- cmd/main.go
+++ cmd/main.go
@@ -51,7 +51,7 @@ import (
 
 var (
 	// Version information - set during build
-	version = "v0.6.8"
+	version = "v0.6.9"
 	commit  = "unknown"
 	date    = "unknown"
 
